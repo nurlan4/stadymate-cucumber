ca0f07de7299db0f0999abde8e57d31fc440daca
Isakazy Amanbaev <isakazy@gmai.com>
2023-07-04 14:22:54 -0600

    commit added logs

---
+++ src/test/java/steps/HooksTest.java
@@ -0,0 +1,28 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
+
+public class HooksTest {
+    final static Logger logger = Logger.getLogger(HooksTest.class);
+    @Before
+    public void before(Scenario scenario){
+        logger.info(scenario+" is starting");
+    }
+    @After
+    public void after(Scenario scenario){
+        if(scenario.isFailed()){
+            logger.error( scenario +" failed!");
+            byte [] screenshot = ((TakesScreenshot)(Driver.getDriver())).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+        else {logger.info(scenario + " passed");}
+        Driver.quit();
+    }
+
+}
